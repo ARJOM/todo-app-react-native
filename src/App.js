fdeb8c72f8c78de03357e8068c9922e7d44be35e
Ricart <ricarteusuo11@gmail.com>
2020-06-01 14:47:32 -0300

    feat: adiciona roteamento

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Home from "./pages/Home";
+import Routes from "./routes";
 
 export default function App() {
   return (
-      <Home/>
+      <Routes/>
   )
 }
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from "./pages/Home";
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Home" component={Home}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
