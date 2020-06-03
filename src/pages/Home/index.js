a0e5757fad21ab7245bf80660a3a29d7848bcb90
Ricart <ricarteusuo11@gmail.com>
2020-06-03 00:24:11 -0300

    view: adiciona estilização na tela inicial

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {TouchableOpacity, View, Text} from 'react-native';
+import styles from "./styles";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Aplicativo To Do para praticar react-native</Text>
-    </View>
+      <View style={styles.container}>
+          <Text style={styles.description}>Aplicativo To Do para praticar react-native</Text>
+          <TouchableOpacity
+              style={styles.button}
+              onPress={() => {}}
+          >
+              <Text>Começar</Text>
+          </TouchableOpacity>
+      </View>
   );
-}
+};
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 24,
+        backgroundColor: '#022B3A'
+    },
+
+    description: {
+        fontSize: 50,
+        color: '#FFFFFF',
+    },
+
+    button: {
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10,
+        width: 200
+    }
+
+
+})
